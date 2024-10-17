3c932bcafbad0e50dea46950a8c2a12bce5cc712
Ирина <barhatov4.i@yandex.ru>
2024-10-17 21:38:08 +0300

    Реализовать ТЗ 15 спринта.

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,7 +2,7 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class ShareItTests {
 
@@ -10,4 +10,4 @@ class ShareItTests {
 	void contextLoads() {
 	}
 
-}
+} */
