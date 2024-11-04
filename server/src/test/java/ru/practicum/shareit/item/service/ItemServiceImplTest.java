73e2e31925983a58f530c927ac0d35fb55d0e7a9
Ирина <barhatov4@yandex.ru>
2024-11-04 17:41:45 +0300

    Реализовать ТЗ 16 спринта.

---
+++ server/src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
@@ -0,0 +1,109 @@
+package ru.practicum.shareit.item.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.dto.CommentDto;
+
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.repository.CommentRepository;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repository.ItemRepository;
+
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@ActiveProfiles("test")
+class ItemServiceImplTest {
+
+    ItemRequest itemRequest;
+    ItemService itemService;
+    UserRepository userRepository;
+    ItemRepository itemRepository;
+    CommentRepository commentRepository;
+    LocalDateTime now = LocalDateTime.now();
+
+    @BeforeEach
+    void setUp() {
+        itemRepository = mock(ItemRepository.class);
+        userRepository = mock(UserRepository.class);
+    }
+
+    @Test
+    void test_getItemWithBookingAndComment() {
+        CommentDto inputCommentDto = CommentDto.builder().id(1).text("new comment for test").build();
+
+        User owner2 = User.builder()
+                .id(2)
+                .name("name1")
+                .email("name1@yandex.ru")
+                .build();
+
+        User userForTest2 = User.builder()
+                .id(1)
+                .name("name2")
+                .email("name2@yandex.ru")
+                .build();
+
+        Item item1 = Item.builder()
+                .id(1)
+                .name("item1")
+                .description("desc item1")
+                .ownerId(owner2.getId())
+                .build();
+
+        Booking bookingFromBd = Booking.builder()
+                .id(1)
+                .item(item1)
+                .booker(userForTest2)
+                .start(now.minusDays(10))
+                .end(now.minusDays(5))
+                .build();
+
+        Item itemFromBd = Item.builder()
+                .id(1)
+                .name("name")
+                .description("desc")
+                .ownerId(owner2.getId())
+                .available(true)
+                .build();
+
+        CommentDto commentDto = CommentDto.builder()
+                .id(1)
+                .text("text1")
+                .authorName("name1")
+                .created(now.minusDays(5))
+                .build();
+
+        Comment outputComment = Comment.builder()
+                .id(1)
+                .author(userForTest2)
+                .text("text2")
+                .item(itemFromBd)
+                .build();
+
+        UserRepository userRepositoryJpa2 = mock(UserRepository.class);
+        ItemRepository itemRepositoryJpa2 = mock(ItemRepository.class);
+        CommentRepository commentRepository2 = mock(CommentRepository.class);
+
+        when(userRepositoryJpa2.findById(any())).thenReturn(Optional.of(userForTest2));
+        when(itemRepositoryJpa2.findById(any())).thenReturn(Optional.of(itemFromBd));
+        when(commentRepository2.save(any())).thenReturn(outputComment);
+    }
+}
---
+++ server/src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
@@ -0,0 +1,58 @@
+package ru.practicum.shareit.request.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ItemRequestMapperTest {
+    @Test
+    void test_toItemRequestDto() {
+        User user = new User();
+        user.setId(1);
+        user.setName("name");
+        user.setEmail("name@yandex.ru");
+
+        ItemRequest itemRequest = new ItemRequest();
+        itemRequest.setId(1);
+        itemRequest.setDescription("desc");
+        itemRequest.setCreator(user);
+        itemRequest.setCreated(LocalDateTime.now());
+
+        ItemRequestDto result = ItemRequestMapper.toItemRequestDto(itemRequest);
+
+        assertNotNull(result);
+        assertEquals(itemRequest.getId(), result.getId());
+        assertEquals(itemRequest.getDescription(), result.getDescription());
+        assertEquals(itemRequest.getCreated(), result.getCreated());
+    }
+
+    @Test
+    void test_toItemRequestResponseDto() {
+        User user = new User();
+        user.setId(1);
+        user.setName("name1");
+        user.setEmail("name1@yandex.ru");
+
+        ItemRequest itemRequest = new ItemRequest();
+        itemRequest.setId(1);
+        itemRequest.setDescription("Описание предмета");
+        itemRequest.setCreator(user);
+        itemRequest.setCreated(LocalDateTime.now());
+
+        ItemRequestDto result = ItemRequestMapper.toItemRequestDto(itemRequest);
+
+        assertNotNull(result);
+        assertEquals(itemRequest.getId(), result.getId());
+        assertEquals(itemRequest.getDescription(), result.getDescription());
+        assertEquals(itemRequest.getCreator().getId(), result.getRequestor().getId());
+        assertEquals(itemRequest.getCreated(), result.getCreated());
+    }
+}
